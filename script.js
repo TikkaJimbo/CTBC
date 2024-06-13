2a0ec7371abe6e7d91010b663192cb8361751432
TikkaJimbo <156189478+TikkaJimbo@users.noreply.github.com>
2024-06-13 22:51:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     const basicStats = [
-        { Player: 'Janna', Runs: 28, 'Balls Faced': 32, 'Not Outs': 2, Avg: 9.33, SR: 87.5, 'Dot Ball %': 37.5 },
-        { Player: 'Oscar', Runs: 20, 'Balls Faced': 22, 'Not Outs': 1, Avg: 4, SR: 90.9, 'Dot Ball %': 54.54 },
-        { Player: 'Weera', Runs: 34, 'Balls Faced': 42, 'Not Outs': 1, Avg: 8.5, SR: 80.95, 'Dot Ball %': 69 },
-        { Player: 'Tikka', Runs: 43, 'Balls Faced': 29, 'Not Outs': 1, Avg: 10.75, SR: 148.3, 'Dot Ball %': 51.72 },
-        { Player: 'Ranul', Runs: 55, 'Balls Faced': 30, 'Not Outs': 1, Avg: 18.3, SR: 183.3, 'Dot Ball %': 33.3 },
-        { Player: 'Navin', Runs: 19, 'Balls Faced': 26, 'Not Outs': 1, Avg: 4.75, SR: 73.1, 'Dot Ball %': 50 },
-        { Player: 'Amila', Runs: 6, 'Balls Faced': 13, 'Not Outs': 0, Avg: 3, SR: 46.2, 'Dot Ball %': 53.8 },
-        { Player: 'Vidura', Runs: 7, 'Balls Faced': 5, 'Not Outs': 0, Avg: 3.5, SR: 140, 'Dot Ball %': 60 }, 
-        { Player: 'Pamodh', Runs: 2, 'Balls Faced': 9, 'Not Outs': 0, Avg: 1, SR: 22.2, 'Dot Ball %': 77.8 },
+        { Player: 'Janna', Runs: 43, 'Balls Faced': 42, 'Not Outs': 2, Avg: 8.6, SR: 102.4, 'Dot Ball %': 40.5 },
+        { Player: 'Oscar', Runs: 83, 'Balls Faced': 57, 'Not Outs': 4, Avg: 11.9, SR: 145.6, 'Dot Ball %': 47.4 },
+        { Player: 'Weera', Runs: 79, 'Balls Faced': 67, 'Not Outs': 2, Avg: 9.9, SR: 118, 'Dot Ball %': 59.7 },
+        { Player: 'Tikka', Runs: 66, 'Balls Faced': 49, 'Not Outs': 3, Avg: 11, SR: 134.7, 'Dot Ball %': 46.9 },
+        { Player: 'Ranul', Runs: 143, 'Balls Faced': 67, 'Not Outs': 2, Avg: 23.8, SR: 213.4, 'Dot Ball %': 28.4 },
+        { Player: 'Navin', Runs: 28, 'Balls Faced': 37, 'Not Outs': 1, Avg: 4, SR: 75.7, 'Dot Ball %': 48.6 },
+        { Player: 'Amila', Runs: 20, 'Balls Faced': 29, 'Not Outs': 2, Avg: 5, SR: 69, 'Dot Ball %': 51.7 },
+        { Player: 'Vidura', Runs: 15, 'Balls Faced': 17, 'Not Outs': 1, Avg: 3.75, SR: 88.2, 'Dot Ball %': 70.6 }, 
         { Player: 'Ishan', Runs: 0, 'Balls Faced': 4, 'Not Outs': 0, Avg: 0, SR: 0, 'Dot Ball %': 100 },
-        { Player: 'Hasintha', Runs: 3, 'Balls Faced': 14, 'Not Outs': 1, Avg: 3, SR: 21.42, 'Dot Ball %': 45 }
+        { Player: 'Pamodh', Runs: 38, 'Balls Faced': 35, 'Not Outs': 3, Avg: 9.5, SR: 108.6, 'Dot Ball %': 51.4 },
+        { Player: 'Fonny', Runs: 16, 'Balls Faced': 14, 'Not Outs': 1, Avg: 16, SR: 114.3, 'Dot Ball %': 42.9},
+        { Player: 'Hasintha', Runs: 3, 'Balls Faced': 14, 'Not Outs': 1, Avg: 3, SR: 21.43, 'Dot Ball %': 78.6 }
     ];
 
     const advancedStats = [
-        { Player: 'Janna', '% Middled': 65.3, '% Edged': 15.6, '% Missed': 12.5, '% Runs in Boundaries': 35.7, '% Boundary Balls': 6.25, 'Death Over SR': 122.2, 'PP SR': 0, 'LMS SR': 100, 'LMS Avg': 8 },
-        { Player: 'Oscar', '% Middled': 59.09, '% Edged': 22.72, '% Missed': 18.18, '% Runs in Boundaries': 60, '% Boundary Balls': 9.09, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
-        { Player: 'Weera', '% Middled': 45.23, '% Edged': 30.95, '% Missed': 21.42, '% Runs in Boundaries': 76.47, '% Boundary Balls': 11.9, 'Death Over SR': 230, 'PP SR': 0, 'LMS SR': 166.6, 'LMS Avg': 25 },
-        { Player: 'Tikka', '% Middled': 44.8, '% Edged': 27.58, '% Missed': 27.58, '% Runs in Boundaries': 79.06, '% Boundary Balls': 20.68, 'Death Over SR': 137.5, 'PP SR': 0, 'LMS SR': 157.1, 'LMS Avg': 5.5 },
-        { Player: 'Ranul', '% Middled': 70, '% Edged': 30, '% Missed': 0, '% Runs in Boundaries': 76.3, '% Boundary Balls': 26.6, 'Death Over SR': 233.3, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
-        { Player: 'Navin', '% Middled': 50, '% Edged': 23.07, '% Missed': 26.92, '% Runs in Boundaries': 31.57, '% Boundary Balls': 3.84, 'Death Over SR': 120, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
-        { Player: 'Amila', '% Middled': 30.76, '% Edged': 53.84, '% Missed': 15.34, '% Runs in Boundaries': 0, '% Boundary Balls': 0, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
-        { Player: 'Vidura', '% Middled': 100, '% Edged': 0, '% Missed': 0, '% Runs in Boundaries': 85.71, '% Boundary Balls': 20, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
+        { Player: 'Janna', '% Middled': 64.3, '% Edged': 16.7, '% Missed': 11.9, '% Runs in Boundaries': 51.2, '% Boundary Balls': 9.52, 'Death Over SR': 122.2, 'PP SR': 0, 'LMS SR': 100, 'LMS Avg': 8 },
+        { Player: 'Oscar', '% Middled': 57.9, '% Edged': 17.54, '% Missed': 24.6, '% Runs in Boundaries': 74.7, '% Boundary Balls': 19.29, 'Death Over SR': 250, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
+        { Player: 'Weera', '% Middled': 54.7, '% Edged': 26.9, '% Missed': 17.9, '% Runs in Boundaries': 78.48, '% Boundary Balls': 16.41, 'Death Over SR': 209, 'PP SR': 0, 'LMS SR': 166.6, 'LMS Avg': 25 },
+        { Player: 'Tikka', '% Middled': 46.9, '% Edged': 28.6, '% Missed': 24.5, '% Runs in Boundaries': 69.7, '% Boundary Balls': 16.3, 'Death Over SR': 137.5, 'PP SR': 0, 'LMS SR': 157.1, 'LMS Avg': 5.5 },
+        { Player: 'Ranul', '% Middled': 70.1, '% Edged': 26.9, '% Missed': 2.99, '% Runs in Boundaries': 76.3, '% Boundary Balls': 26.6, 'Death Over SR': 273.3, 'PP SR': 0, 'LMS SR': 433.3, 'LMS Avg': 13 },
+        { Player: 'Navin', '% Middled': 56.8, '% Edged': 18.9, '% Missed': 21.6, '% Runs in Boundaries': 35.7, '% Boundary Balls': 5.4, 'Death Over SR': 120, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
+        { Player: 'Amila', '% Middled': 44.8, '% Edged': 34.48, '% Missed': 20.7, '% Runs in Boundaries': 30, '% Boundary Balls': 3.45, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
+        { Player: 'Vidura', '% Middled': 58.8, '% Edged': 23.5, '% Missed': 17.6, '% Runs in Boundaries': 80, '% Boundary Balls': 11.8, 'Death Over SR': 25, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
         { Player: 'Ishan', '% Middled': 50, '% Edged': 25, '% Missed': 25, '% Runs in Boundaries': 0, '% Boundary Balls': 0, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
-        { Player: 'Pamodh', '% Middled': 55.55, '% Edged': 33.33, '% Missed': 11.11, '% Runs in Boundaries': 0, '% Boundary Balls': 0, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
+        { Player: 'Pamodh', '% Middled': 68.6, '% Edged': 36.8, '% Missed': 14.3, '% Runs in Boundaries': 63.2, '% Boundary Balls': 11.4, 'Death Over SR': 112.5, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
+        { Player: 'Fonny', '% Middled': 57.14, '% Edged': 21.4, '% Missed': 14.3, '% Runs in Boundaries': 62.5, '% Boundary Balls': 7.14, 'Death Over SR': 200, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 },
         { Player: 'Hasintha', '% Middled': 35.71, '% Edged': 7.14, '% Missed': 57.14, '% Runs in Boundaries': 0, '% Boundary Balls': 0, 'Death Over SR': 0, 'PP SR': 0, 'LMS SR': 0, 'LMS Avg': 0 }
     ];
 
     const rankingMetrics = [
-        { Player: 'Janna', OBS: 3.35, BCR: 0.14, BDF: 3.57, CPI: 0.97, SE: 1.66, SRE: 0.6, PPI: 0, DOI: 8.78 },
-        { Player: 'Oscar', OBS: 1.33, BCR: 0.34, BDF: 1.15, CPI: 0, SE: 1.32, SRE: 0.4, PPI: 0, DOI: 0 },
-        { Player: 'Weera', OBS: 5.44, BCR: 0.57, BDF: 1.49, CPI: 6.05, SE: 0.79, SRE: 0.22, PPI: 0, DOI: 30.75 },
-        { Player: 'Tikka', OBS: 4.10, BCR: 1.02, BDF: 5.38, CPI: 0.54, SE: 0.73, SRE: 0.39, PPI: 0, DOI: 3.79 },
-        { Player: 'Ranul', OBS: 8.57, BCR: 1.27, BDF: 15.68, CPI: 0, SE: 1.8, SRE: 0.59, PPI: 0, DOI: 2.59 },
-        { Player: 'Navin', OBS: 2.00, BCR: 0.076, BDF: 1.21, CPI: 0, SE: 0.94, SRE: 0.52, PPI: 0, DOI: 8.29 },
-        { Player: 'Amila', OBS: 0.66, BCR: 0, BDF: 0.44, CPI: 0, SE: 0.2, SRE: 0.46, PPI: 0, DOI: 0 },
-        { Player: 'Vidura', OBS: 2.17, BCR: 1.07, BDF: 1.37, CPI: 0, SE: 3, SRE: 0.25, PPI: 0, DOI: 0 },
+        { Player: 'Janna', OBS: 2.776, BCR: 0.305, BDF: 3.669, CPI: 0.909, SE: 1.619, SRE: 0.553, PPI: 0, DOI: 1.527 },
+        { Player: 'Oscar', OBS: 4.184, BCR: 0.901, BDF: 6.361, CPI: 0, SE: 1.267, SRE: 0.457, PPI: 0, DOI: 1.551 },
+        { Player: 'Weera', OBS: 3.330, BCR: 0.805, BDF: 3.285, CPI: 3.578, SE: 1.128, SRE: 0.304, PPI: 0, DOI: 2.581 },
+        { Player: 'Tikka', OBS: 3.656, BCR: 0.711, BDF: 5.503, CPI: 0.583, SE: 0.829, SRE: 0.488, PPI: 0, DOI: 0.851 },
+        { Player: 'Ranul', OBS: 12.855, BCR: 1.461, BDF: 25.510, CPI: 1.107, SE: 1.8, SRE: 0.660, PPI: 0, DOI: 1.836 },
+        { Player: 'Navin', OBS: 1.365, BCR: 0.121, BDF: 1.088, CPI: 0, SE: 1.254, SRE: 0.514, PPI: 0, DOI: 1.699 },
+        { Player: 'Amila', OBS: 1.106, BCR: 0.065, BDF: 1.165, CPI: 0, SE: 0.752, SRE: 0.5, PPI: 0, DOI: 0 },
+        { Player: 'Vidura', OBS: 1.202, BCR: 0.588, BDF: 0.681, CPI: 0, SE: 1.318, SRE: 0.2, PPI: 0, DOI: 0.094 },
         { Player: 'Ishan', OBS: 0.095, BCR: 0, BDF: 0, CPI: 0, SE: 0.95, SRE: 0, PPI: 0, DOI: 0 },
-        { Player: 'Pamodh', OBS: 0.35, BCR: 0, BDF: 0.03, CPI: 0, SE: 1.2, SRE: 0.22, PPI: 0, DOI: 0 },
+        { Player: 'Fonny', OBS: 4.213, BCR: 0.167, BDF: 7.314, CPI: 0, SE: 1.314, SRE: 0.769, PPI: 0, DOI: 2.188 },
+        { Player: 'Pamodh', OBS: 2.600, BCR: 0.451, BDF: 3.507, CPI: 0, SE: 1.714, SRE: 0.452, PPI: 0, DOI: 1.227 },
         { Player: 'Hasintha', OBS: 0.24, BCR: 0, BDF: 0, CPI: 0, SE: 0.31, SRE: 0.21, PPI: 0, DOI: 0 }
     ];
 
